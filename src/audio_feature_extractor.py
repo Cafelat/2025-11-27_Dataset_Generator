65c6502466c018710a0290f0768e9328c224c996
Cafelat <s98085115@gmail.com>
2025-11-28 09:46:10 +0900

    add superimpose method

--- src/audio_feature_extractor.py
+++ src/audio_feature_extractor.py
@@ -1,4 +1,5 @@
-
+import copy
+import warnings
 
 import librosa
 import matplotlib.pyplot as plt
@@ -21,6 +22,37 @@ class TimeSeriesData:
     def load_audio_file(cls, path, sr=None):
         data, sr = librosa.load(path, sr=sr, res_type='soxr_vhq')
         return cls(data, sr)
+
+    @classmethod
+    def superimpose(cls, signal, noise, snr_db):
+        
+        if signal.sr != noise.sr:
+            raise ValueError("Sampling rates of signal and noise must match.")
+        
+        if len(signal.data) != len(noise.data):
+            warnings.warn("Signal and noise lengths differ. Truncating to the shorter length.", UserWarning, stacklevel=2)
+
+        min_length = min(len(signal.data), len(noise.data))
+        signal_data = signal.data[:min_length].copy()
+        noise_data  = noise.data[:min_length].copy()
+        
+        signal_rms  = np.sqrt(np.mean(signal_data**2))
+        noise_rms   = np.sqrt(np.mean(noise_data**2))
+        
+        desired_noise_rms = signal_rms / (10**(snr_db / 20))
+        scaling_factor = desired_noise_rms / (noise_rms + 1e-10)
+        adjusted_noise_data = noise_data * scaling_factor
+        adjusted_noisy_data = signal_data + adjusted_noise_data
+        adjusted_signal_data = signal_data
+
+        max_abs = np.max(np.abs(adjusted_noisy_data))
+        if max_abs > 1.0:
+            adjusted_noisy_data = adjusted_noisy_data / max_abs
+            adjusted_signal_data = adjusted_signal_data / max_abs
+            adjusted_noise_data  = adjusted_noise_data / max_abs
+        
+        return cls(adjusted_noisy_data, signal.sr), cls(adjusted_signal_data, signal.sr), cls(adjusted_noise_data, signal.sr)
+
 
     def plot(self, ax=None, duration=None, **kwargs):
         
